4ae10c194f3acd12c1220c05ba7dfe094cd84da6
clean2001 <clearrworld@gmail.com>
2024-07-21 21:17:45 +0900

    feat: 모달창

--- src/App.vue
+++ src/App.vue
@@ -1,26 +1,26 @@
-<template>
-  <img alt="Vue logo" src="./assets/logo.png">
-  <HelloWorld msg="Welcome to Your Vue.js App"/>
+<template> <!-- 템플릿 속성 안에는 html 코드가 들어간다 -->
+  <v-app>
+    <v-main>
+      <!-- <HelloWorld/> -->
+      <ModalDialog/>
+    </v-main>
+  </v-app>
 </template>
 
 <script>
-import HelloWorld from './components/HelloWorld.vue'
+// import HelloWorld from './components/HelloWorld.vue'
+import ModalDialog from './components/ModalDialog.vue';
 
-export default {
+export default { // export default로 모듈을 추출하고 있다.
   name: 'App',
+
   components: {
-    HelloWorld
-  }
+    // HelloWorld,
+    ModalDialog
+  },
+
+  data: () => ({
+    //
+  }),
 }
 </script>
-
-<style>
-#app {
-  font-family: Avenir, Helvetica, Arial, sans-serif;
-  -webkit-font-smoothing: antialiased;
-  -moz-osx-font-smoothing: grayscale;
-  text-align: center;
-  color: #2c3e50;
-  margin-top: 60px;
-}
-</style>
---
+++ src/components/ModalDialog.vue
@@ -0,0 +1,46 @@
+<template>
+  <div>
+    <v-btn color="primary" dark @click="dialog = true">Open Modal</v-btn>
+
+    <v-dialog v-model="dialog" max-width="500px">
+      <v-card>
+        <v-card-title>
+          <span class="headline">Modal Title</span>
+        </v-card-title>
+
+        <v-card-text>
+          This is the content of the modal.
+        </v-card-text>
+
+        <v-card-actions>
+          <v-spacer></v-spacer>
+          <v-btn color="blue darken-1" text @click="dialog = false">Close</v-btn>
+          <v-btn color="blue darken-1" text @click="save">Save</v-btn>
+        </v-card-actions>
+      </v-card>
+    </v-dialog>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      dialog: false
+    };
+  },
+  methods: {
+    save() {
+      // Save action
+      this.dialog = false;
+      console.log('Save action');
+    }
+  }
+};
+</script>
+
+<style scoped>
+.headline {
+  font-weight: bold;
+}
+</style>
